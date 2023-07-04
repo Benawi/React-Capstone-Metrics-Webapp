97e4cac5a20c7f52b6365707a8ed1afc70d6fbca
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-04 11:30:24 +0300

    Add feach total cases

--- src/redux/fetechers/fetchCountries.js
+++ src/redux/fetechers/fetchCountries.js
@@ -3,4 +3,9 @@ import axios from 'axios';
 const fetchCountriesCases = async () => axios.get('https://disease.sh/v3/covid-19/countries')
   .then((response) => response.data);
 
+const fetchTotalCases = async () => axios.get('https://disease.sh/v3/covid-19/all')
+  .then((response) => response.data.cases);
+
+
+
 export default fetchCountries;
