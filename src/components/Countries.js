fc5381aa25d0993407d01181719e3a3eacd9b3aa
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-04 14:40:12 +0300

    dispatch the countries list

---
+++ src/components/Countries.js
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import getCountries from '../redux/thunk/countries';
+import Header from './Navigation';
+import CountryItem from './CountryList';
+
+const Countries = () => {
+  const { countries, totalTodayConfirmed } = useSelector((state) => state.countries);
+  const [countriesToShow, setCountriesToShow] = useState([]);
+  let backgroudColorForCountry = '#dc4782';
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCountries());
+  }, []);
+
+ 
