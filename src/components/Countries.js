4d602571b75371223c91db388a8634fbdb83f81b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-04 14:41:36 +0300

    handle searching for an item

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -31,57 +31,7 @@ const Countries = () => {
     }
   };
 
-  useEffect(() => {
-    if (countries !== undefined) {
-      setCountriesToShow(countries);
-    }
-  }, [countries]);
-
-  return (
-    <>
-      <Header page="countries" />
-      {(countriesToShow === undefined || totalTodayConfirmed === undefined) && (
-        <div className="loading">
-          <div className="loader" />
-        </div>
-      )}
-      {!(countriesToShow === undefined || totalTodayConfirmed === undefined) && (
-        <div className="main-container">
-          <div className="total-countries-div">
-            <i className="fas fa-globe-americas fa-5x" />
-            <div>
-              <span>All Countries</span>
-              <span>{`${totalTodayConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} Cases`}</span>
-            </div>
-          </div>
-          <div className="cases-by-countries">
-            <p>CASES BY COUNTRIES</p>
-            <div className="filter-inputs">
-              <input type="text" id="search-input" onChange={handleSearch} placeholder="Search" />
-            </div>
-          </div>
-          <div className="countries">
-            {countriesToShow.map((country, index) => {
-              if ((index + 1) % 2 === 0) {
-                if (backgroudColorForCountry === '#dc4782') {
-                  backgroudColorForCountry = '#cf4479';
-                } else {
-                  backgroudColorForCountry = '#dc4782';
-                }
-              }
-              return (
-                <CountryItem
-                  key={country.country}
-                  country={country}
-                  backgroundColor={backgroudColorForCountry}
-                />
-              );
-            })}
-          </div>
-        </div>
-      )}
-    </>
-  );
+ 
 };
 
 export default Countries;
