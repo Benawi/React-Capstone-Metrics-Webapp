fbe1bf65b416225116d645d6fc9705f9146edf52
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-04 14:15:57 +0300

    Fix linter error

--- src/redux/fetechers/fetchCountries.js
+++ src/redux/fetechers/fetchCountries.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-const URLC='https://disease.sh/v3/covid-19/countries';
-const URLA='https://disease.sh/v3/covid-19/all';
+
+const URLC = 'https://disease.sh/v3/covid-19/countries';
+const URLA = 'https://disease.sh/v3/covid-19/all';
 const fetchCountriesCases = async () => axios.get(URLC)
   .then((response) => response.data);
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import thunk from './redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import countriesReducer from './reducers/countries';
 import regionsReducer from './reducers/regions';
 
-const reducer = combineReducers({
-  countries: countriesReducer,
-  regions: regionsReducer,
+const Store = configureStore({
+  reducer: {
+    countries: countriesReducer,
+    regions: regionsReducer,
+  },
 });
 
-const store = createStore(
-  reducer,
-  applyMiddleware(logger, thunk),
-);
-
-export default store;
+export default Store;
