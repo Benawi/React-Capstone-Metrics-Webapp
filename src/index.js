61e6fb63cbf8a404b595589a173c2a6f5e941395
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-07-05 22:44:39 +0300

    Add base name for deployement

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/React-Capstone-Metrics-webapp">
         <App />
       </BrowserRouter>
     </Provider>
